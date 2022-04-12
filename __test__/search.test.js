c3c019abb447a0e67186b30213e02421739d62d7
Artem Rakov <artem@rakov.me>
2022-04-12 05:31:54 +0300

    simple search

--- __test__/search.test.js
+++ __test__/search.test.js
@@ -6,3 +6,12 @@ test('empty search', () => {
 
   expect(searchEngine.search('')).toEqual([]);
 });
+
+test('search', () => {
+  const doc1 = { id: 'doc1', text: "I can't shoot straight unless I've had a pint!" };
+  const doc2 = { id: 'doc2', text: "Don't shoot shoot shoot that thing at me." };
+  const doc3 = { id: 'doc3', text: "I'm your shooter." };
+  const searchEngine = buildSearchEngine([doc1, doc2, doc3]);
+
+  expect(searchEngine.search('shoot')).toEqual(['doc1', 'doc2']);
+});
--- src/search.js
+++ src/search.js
@@ -1,9 +1,19 @@
-// @ts-check
+const search = (docs, target) => {
+  const result = [];
 
-const buildSearchEngine = () => ({
-  search() {
-    return [];
-  },
+  for (let i = 0; i < docs.length; i += 1) {
+    const { id, text } = docs[i];
+
+    if (text.split(' ').includes(target)) {
+      result.push(id);
+    }
+  }
+
+  return result;
+};
+
+const buildSearchEngine = (docs) => ({
+  search: (target) => search(docs, target),
 });
 
 export default buildSearchEngine;
